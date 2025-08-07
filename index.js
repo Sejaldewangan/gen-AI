31b398c24bec3a8b8c518ea619c88ab75d91c863
sejaldewangan <shubham.dewangan.3440@gmail.com>
2025-08-07 18:01:14 +0530

    ai2

--- index.js
+++ index.js
@@ -2,12 +2,12 @@ import Groq from "groq-sdk";
 const groq = new Groq({ apiKey: process.env.GROQ_API_KEY });
 
 async function ai() {
+  const question = await fetch()
   const completion = await groq.chat.completions.create({
     messages: [
       {
         role: "user",
-        content: `hi
-        `,
+        content: `${question}`,
       },
     ],
     model: "llama-3.3-70b-versatile",
@@ -34,19 +34,28 @@ async function ai() {
   //   console.log(chatCompletion.choices[0]?.message?.content || "");
   // });
   console.log(JSON.stringify(completion.choices[0], null, 2));
+
+
  const toolCalls= completion.choices[0].message.tool_calls
 if (!toolCalls) {
   console.log(`Assistant: ${completion.choices[0].message.content}`)
   return
 }
 
+for( const tool of toolCalls){
+  const functionName =tool.function.name
+  const functionArguments =tool.function.arguments
+  if (functionName==='timeManagement') {
+    const result =timeManagement(functionArguments)
+  }
+}
 }
 
 ai();
 
 function timeManagement(form, to) {
   console.log("time is comming");
-  // return db here
+  // return db here mogo ya vector0
   return " 12 hours left";
 }
 // timeManagement();
